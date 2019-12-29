f2b400c0fcde5c33fbaf4d6916fe6921969f4352
mascodigit <emmanuelikennaogbonna@gmail.com>
2019-12-29 17:17:47 +0100

    added settings

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ use App\Areas;
 use App\Vendor;
 use App\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;  
 use Illuminate\Support\Facades\Response;
 

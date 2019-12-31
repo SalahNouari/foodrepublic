4b2d0c3efc7f913e10be6b5afdffbb6406fb73fd
mascodigit <emmanuelikennaogbonna@gmail.com>
2019-12-31 23:52:09 +0100

    all delivery

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -30,7 +30,7 @@ class OrderController extends Controller
 
     public function alldelivery()
     {
-        $order = Auth::user()->delivery_agent->with('orders')->get();
+        $order = Auth::user()->delivery_agent->orders()->latest()->paginate(12);
         $response = [
             'orders' => $order
         ];

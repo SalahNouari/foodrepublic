4dca6ee6acbcc22737890da6b40bfec387361555
mascodigit <emmanuelikennaogbonna@gmail.com>
2020-01-03 21:33:56 +0100

    err

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -142,7 +142,7 @@ public $successStatus = 200;
                 //     $user->save();
                 //     $success['user'] = ['phone' => $user->phone, 'type' => 'phone'];
                 // } else {
-                    $success['error'] =  $result['data'];
+                    $success['error'] =  $result['data']->SMSMessageData;
                     return response()->json(['success'=>$success], $this-> successStatus); 
                      
                 //  }

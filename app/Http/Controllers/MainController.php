2af7c7fb4b8fa5ee32660959fe4b705668527037
mascodigit <emmanuelikennaogbonna@gmail.com>
2020-01-23 12:55:58 +0100

    d

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -27,14 +27,15 @@ class MainController extends Controller
     {
 
         $d = Areas::find($request->id);
-        $vendor = $d->vendor()->with( [
+        $vendor = $d->vendor()
+        ->select('name', 'cash_on_delivery', 'lat', 'lng', 'card_on_delivery', 'vendor_id', 'image')
+        ->with( [
         'tags'  => function ($query) {
                 $query->select('tag');
             },
          'area' => function ($query) use ($request) {
             $query->where('areas_id', $request->id);
         }])
-        ->select('name', 'cash_on_delivery', 'lat', 'lng', 'card_on_delivery', 'vendor_id', 'image')
         ->get();
         $response = [
             'items' => $vendor

dcbeaeba28fd50b0a9315d158d9e2285fe838dc2
mascodigit <emmanuelikennaogbonna@gmail.com>
2020-02-18 19:42:24 +0100

    d

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -37,7 +37,7 @@ class MainController extends Controller
                 $query->where('areas_id', $request->id);
             },
              'reviews' => function ($query){
-                $query->select(DB::raw('sum(rating) as rate, vendor_id, id'));
+                $query->select(DB::raw('sum(rating) as rate'));
             }
             ])
         ->withCount('reviews')

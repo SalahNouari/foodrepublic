18463fdb009b0ca0b55d69725d633f4ce3cfa054
mascodigit <emmanuelikennaogbonna@gmail.com>
2020-05-28 08:18:35 +0100

    dd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -22,7 +22,8 @@ class OrderController extends Controller
 {
     public function all(Request $request)
     {
-        $order = Auth::user()->vendor->orders()->latest()->makeHidden(['change_amount', 'distance', 'duration', 'grand_total', 'service_charge', 'delivery_fee', 'delivered_time', 'recieved_time', 'reject_reason', 'rejected_time', 'served_time', 'transit_time', 'table_no', 'total'])->paginate(12);
+        $order = Auth::user()->vendor->orders()->latest()->paginate(12);
+        $order['data']->makeHidden(['change_amount', 'distance', 'duration', 'grand_total', 'service_charge', 'delivery_fee', 'delivered_time', 'recieved_time', 'reject_reason', 'rejected_time', 'served_time', 'transit_time', 'table_no', 'total']);
         $response = [
             'orders' => $order
         ];

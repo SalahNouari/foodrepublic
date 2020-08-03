4a2e186ee7524d026f7651a15ff99da8f12fb44b
2bona <sayhitobona@gmail.com>
2020-08-03 01:15:41 +0100

    delivery monitor setup

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -32,7 +32,7 @@ class DeliveryController extends Controller
     {
         //when we have many agents and they are assigned to different areas
         // $agents = Delivery::where("current_area_id", $request->id)->select("name", "id", "lat", "lng")->latest()->get();
-        $agents = Delivery::select("name", "id", "lat", "lng")->latest()->get();
+        $agents = Delivery::where('city', $request->id)->select("name", "id", "lat", "lng")->latest()->get();
         $response = [
             'agents' => $agents
         ];

1f3fcd26c7e990ade8616accefc534091b66dee3
2bona <sayhitobona@gmail.com>
2020-08-23 14:29:34 +0100

    dd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -73,7 +73,7 @@ public $successStatus = 200;
         }
     }
     public function load(){
-        $user = Auth::user()->with(['orders'])->get();
+        $user = Auth::user()->with(['orders'])->first();
         $success['user'] =  $user;
         return response()->json(['success' => $success], $this->successStatus);
     }

9e01010c22d56604bb52e40d6b5a58fba2e3fd31
2bona <sayhitobona@gmail.com>
2020-08-24 13:48:47 +0100

    cors correct add api

--- config/cors.php
+++ config/cors.php
@@ -14,6 +14,7 @@ return [
    
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
+    'paths' => ['api/*'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],

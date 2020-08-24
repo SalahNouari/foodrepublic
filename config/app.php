cf64ba2508923c616b61c64ad589ee7844c7dcc7
2bona <sayhitobona@gmail.com>
2020-08-24 17:46:27 +0100

    pusher to config app.php

--- config/app.php
+++ config/app.php
@@ -227,6 +227,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Cloudder' => JD\Cloudder\Facades\Cloudder::class,
+        'Pusher' => Pusher\Pusher::class,
+
     ],
 
 ];

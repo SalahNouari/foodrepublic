7053a65d25419df4f7f28d0c4a5f44a230caa90c
2bona <sayhitobona@gmail.com>
2020-08-26 16:17:12 +0100

    dd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -366,7 +366,7 @@ class OrderController extends Controller
             // Cache::forget('vendor_timer_'.$vendorId);
 
 
-             if(!Cache::has('vendor_timer_'.$vendorId)){
+             if(Cache::has('vendor_timer_'.$vendorId)){
                  $this->Start_timer($vendorId, $vendor, $area);
                  $reply = 'Its now in timer';
                 } else {
@@ -377,7 +377,7 @@ class OrderController extends Controller
                 'message2' => 'Prepare this order, delivery agent is on the way',
                 'token' => $user->token,
                 've' => $reply,
-                'va' => !Cache::has('vendor_timer_'.$vendorId),
+                'va' => Cache::has('vendor_timer_'.$vendorId),
                 "vendorToken" => $vendorToken,
             ];
             return response()->json($response);

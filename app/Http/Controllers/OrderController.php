490731eb6ac0821190bcac4b44425d1f064ec2a5
2bona <sayhitobona@gmail.com>
2020-08-26 16:54:06 +0100

    dd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -371,7 +371,7 @@ class OrderController extends Controller
                   break;
                   default:
                   $this->Start_timer($vendorId, $vendor, $area);
-                  $reply = $vendorId;
+                  $reply = 'vendor_timer_'.$vendorId;
                 }
 
             // if(Cache::has('vendor_timer_'.$vendorId) == true){

a476833d9da860db2585880cacb80f6beb4ee464
2bona <sayhitobona@gmail.com>
2020-08-29 16:23:43 +0100

    added cors police to route middle ware in kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,6 +69,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \Fruitcake\Cors\HandleCors::class
+
     ];
 
     /**

925fd584040fc4be79f6a54afe206b1963793e07
2bona <sayhitobona@gmail.com>
2020-09-02 16:46:54 +0100

    dd

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => true,
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'http://localhost:8080'],
     'paths' => ['api/*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],

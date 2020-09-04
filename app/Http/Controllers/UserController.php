7bb08655f39508875b2d494d1925dd5ca2497255
2bona <sayhitobona@gmail.com>
2020-09-04 12:03:32 +0100

    send token when logged in

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -108,6 +108,7 @@ public $successStatus = 200;
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')-> accessToken; 
             $success['user'] =  $user; 
+            $success['user']['token'] =  $user->token; 
             return response()->json(['success' => $success], $this-> successStatus); 
         }
         else{

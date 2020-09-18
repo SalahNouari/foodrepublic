e98b88252a5970ac293039d6f522ca59169ebb4f
2bona <sayhitobona@gmail.com>
2020-09-18 13:48:29 +0100

    dd

--- routes/channels.php
+++ routes/channels.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('private-orders', function ($user) {
      return Auth::check();
 });
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
+// Broadcast::channel('App.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });

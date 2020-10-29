b912df2dce075dec176f0b522fd1f233295319a2
2bona <sayhitobona@gmail.com>
2020-10-29 11:22:31 +0100

    delete user route

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 
 class Admin extends Controller
 {
+    public function del_user(Request $request){
+        $users = User::find($request->id);
+    
+        $users->delete();
+        $response = [
+            'deleted' => 'deleted'
+        ];
+        return response()->json($response);
+    }
     public function get_users(){
         $users = User::select('id', 'role', 'first_name', 'middle_name', 'surname', 'state_id', 'area_id', 'phone', 'created_at', 'updated_at', 'wallet')
         ->withCount('orders')

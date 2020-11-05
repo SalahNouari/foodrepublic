aaec0d0ed7937d0dea1e12b4d35357f7ae7a751a
2bona <sayhitobona@gmail.com>
2020-11-05 09:55:22 +0100

    dd

--- routes/api.php
+++ routes/api.php
@@ -98,6 +98,7 @@ Route::prefix('transaction')->group(function () {
 Route::prefix('deals')->group(function () {
    Route::post('save', 'DealsController@save')->middleware('auth:api');
    Route::post('add_item', 'DealsController@add_item')->middleware('auth:api');
+   Route::get('remove_deal', 'DealsController@remove_deal')->middleware('auth:api');
    Route::get('remove_item', 'DealsController@remove_item')->middleware('auth:api');
    Route::get('all', 'DealsController@get_deals')->middleware('auth:api');
 });

1097336e2cd5d086649fa13749630f311339658e
2bona <sayhitobona@gmail.com>
2020-11-09 19:02:16 +0100

    JPEG images

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -74,7 +74,7 @@ class VendorController extends Controller
         $files = $request->file('files');
         request()->validate([
             'files' => 'required',
-            'files.*' => 'image|mimes:jpeg,png,JPG,jpg,gif,svg|max:4048'
+            'files.*' => 'image|mimes:JPEG,jpeg,png,JPG,jpg,gif,svg|max:4048'
         ]);
         $vendor = Auth::user()->vendor;
         foreach ($files as $file) {

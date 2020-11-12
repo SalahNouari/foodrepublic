08714f321689b14d5796e1718cef76f4e47014cf
2bona <sayhitobona@gmail.com>
2020-11-12 11:59:10 +0100

    dd

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -55,7 +55,7 @@ class DeliveryController extends Controller
     public function fund(Request $request)
     {
         $rider = Auth::user();
-        if(Auth::attempt(['phone' => $rider->phone, 'password' => request('rider_password')])){ 
+        if(($rider->role === 'delivery_agent') && (Auth::attempt(['phone' => $rider->phone, 'password' => request('rider_password')]))){ 
             $user = User::where('phone', $request->user_phone)->first();
             $rider->increment('funds_collected', $request->amount);
             $user->save();

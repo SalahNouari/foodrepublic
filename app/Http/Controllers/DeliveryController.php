910f7c8585f7f9c2f5008e7523983fb78a88b493
2bona <sayhitobona@gmail.com>
2020-11-13 12:48:04 +0100

    dd

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -42,8 +42,8 @@ class DeliveryController extends Controller
     public function clear_funds_collected(Request $request)
     {
         $admin = Auth::user();
-        if($admin->role === "rider_admin"){ 
-            $rider = Delivery::where('id', $request->id)->first();
+        if($admin->role === 'rider_admin'){ 
+            $rider = Delivery::find($request->id);
             $rider->funds_collected = 0;
             $rider->save();
             return response()->json(['blocked' => false], 200);

0ecb12095fff7ffdd28c944062710f8fae2df03c
2bona <sayhitobona@gmail.com>
2020-12-02 07:40:41 +0100

    home page mta correction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,21 @@
   <!-- Primary Meta Tags -->
 <title>E Dey App</title>
 <meta name="title" content="E Dey App">
-<meta name="description" content="Whether nah restaurant, food stuff, provision, drugs infact that thing wey you dey find , e dey app.">
+<meta name="description" content="Whether nah restaurant, food stuff, provision, drugs infact that thing wey you dey find, e dey app.">
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
-<meta property="og:url" content="https://metatags.io/">
+<meta property="og:url" content="https://edeyapp.com/">
 <meta property="og:title" content="E Dey App">
 <meta property="og:description" content="Whether nah restaurant, food stuff, provision, drugs infact that thing wey you dey find , e dey app.">
-<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+<meta property="og:image" content="https://res.cloudinary.com/dnqw7x4bp/image/upload/v1582290476/e_dey_e_only_2.png">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://metatags.io/">
+<meta property="twitter:url" content="https://edeyapp.com">
 <meta property="twitter:title" content="E Dey App">
 <meta property="twitter:description" content="Whether nah restaurant, food stuff, provision, drugs infact that thing wey you dey find , e dey app.">
-<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+<meta property="twitter:image" content="https://res.cloudinary.com/dnqw7x4bp/image/upload/v1582290476/e_dey_e_only_2.png">
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
